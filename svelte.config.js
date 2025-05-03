c5580c1b6f03fb7296ea17c24ae848b76ba5d849
sasha <sasha.friis@icloud.com>
2025-05-03 19:33:19 +0200

    fixed config and name of server.ts to +server.ts

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,17 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import path from 'path'; // Needed for alias paths
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$utils: path.resolve('./src/utils'),
+			$lib: path.resolve('./src/lib')
+		}
 	}
 };
 
